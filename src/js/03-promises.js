bd307f13836be5884dc0f552c24e0a8bcb5ad7f2
serheihimself <sergm080@gmail.com>
2023-01-14 23:48:45 +0200

    promise dom create commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,6 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnCreatePromise = document.querySelector('button[type="submit"]');
+
