2d9f594d7d43665940e61c4c8a78a7ab7d5e9106
serheihimself <sergm080@gmail.com>
2023-01-14 23:51:38 +0200

    promise foo commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,3 +4,17 @@ const step = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
 const btnCreatePromise = document.querySelector('button[type="submit"]');
 
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
